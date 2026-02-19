24153e0cdb32fcab532ca15da88654c1b2df6f3b
Al Zayan P <alzayanpoothakkal1156@gmail.com>
2026-02-19 10:01:48 +0530

    feat: add reusable JobCard component

--- git-workflow/src/App.jsx
+++ git-workflow/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Badge from './components/Badge'
+import JobCard from './components/JobCard'
 
 const App = () => {
   return (
     <div>
       <Badge/>
+      <JobCard/>
       
     </div>
   )
